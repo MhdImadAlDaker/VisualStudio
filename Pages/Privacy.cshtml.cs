fc2f0fe7ae04a02c6a028450c904df14cd6ce44f
MhdImadAlDaker <MhdImad.AlDaker@my-aolcc.com>
2025-11-03 09:56:53 +0200

    Add files via upload

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mhd_Imad_Al_Daker.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
